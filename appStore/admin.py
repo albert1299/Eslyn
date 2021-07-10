505f3eed0aeacb8a5281dcaa0d0bca8b3989a075
albert1299 <albert.mora@unl.edu.ec>
2021-07-10 14:06:18 -0500

    Nuevos modelos

--- appStore/admin.py
+++ appStore/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from appStore.models import *
 
 # Register your models here.
+admin.site.register(Data_User)
+admin.site.register(Catalog)
 admin.site.register(Product)
--- appStore/models.py
+++ appStore/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
-    size = models.CharField(max_length=10, verbose_name='Talla')
-    color = models.CharField(max_length=25, verbose_name='Color')
-    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
-    description = models.CharField(max_length=300, verbose_name='Descripcion')
-    category = models.CharField(max_length=150, verbose_name='Categoria')
-    state = models.BooleanField(default=True)
+class Data_User(models.Model):
+    ci = models.CharField(max_length=150, verbose_name='Cédula', null=False, blank=False)
+    phone = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Teléfono', null=False, blank=False)
+    address = models.IntegerField(verbose_name='Dirección', null=False, blank=False)
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.ci
+
+class Catalog(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', null=False, blank=False)
+    description = models.CharField(max_length=300, verbose_name='Descripción', null=True, blank=True)
+    #image = models.FileField(max_length=150, verbose_name='Categoria')
+    category = models.CharField(max_length=150, verbose_name='Categoria', null=False, blank=False)
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', null=False, blank=False)
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Precio', null=False, blank=False)
+    stock = models.IntegerField(verbose_name='Stock', null=False, blank=False)
+    description = models.CharField(max_length=300, verbose_name='Descripción', null=True, blank=True)
+    catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 
 
