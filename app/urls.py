3d046cf8c986054865ec4f650e6be795b9c71e24
JosselynGuazha <josselyn.guazha@unl.edu.ec>
2021-07-15 13:42:09 -0500

    login

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from appStore.views.login.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginFormView.as_view()),
     path('store/', include('appStore.urls'))
 ]
 
--- appStore/urls.py
+++ appStore/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from appStore.views.product.views import *
 from appStore.views.catalog.views import *
 
+
 urlpatterns = [
     path('', CatalogListView.as_view(), name='catalogs'),
     path('catalog/products/', ProductListView.as_view(), name='product_list')
