52ac35a4dda76ce65f2dacd40263165e19820d62
JosselynGuazha <josselyn.guazha@unl.edu.ec>
2021-07-23 16:16:12 -0500

    ultimoCambio

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from appStore.views.login.views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginFormView.as_view()),
+    path('login/', auth_views.LoginView.as_view(template_name="login/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='../store/'), name='logout'),
     path('store/', include('appStore.urls'))
 ]
 
--- appStore/urls.py
+++ appStore/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from appStore.views.product.views import *
 from appStore.views.catalog.views import *
+from appStore.views.user.views import *
 
 
 urlpatterns = [
     path('', CatalogListView.as_view(), name='catalogs'),
+    path('createUser/', UsuarioCreateView.as_view(), name= "createUser"),
+    path('createDataUser/<int:pk>/', DataUserCreateView.as_view(), name= "createDataUser"),
     path('catalog/products/', ProductListView.as_view(), name='product_list')
 ]
